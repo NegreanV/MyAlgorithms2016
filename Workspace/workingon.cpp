8bf63eaca17e110fa98229abb7a91dd1ddb76ac9
NegreanV <yo_victor_00@yahoo.com>
2016-01-23 11:16:08 +0200

    Update workingon.cpp

--- Workspace/workingon.cpp
+++ Workspace/workingon.cpp
@@ -8,28 +8,78 @@ int main()
     ifstream inf ("input.in");
     ofstream outf ("output.out");
 
-    int a, b;
-    inf >> a, b;
+    int linie, coloana;
 
-    int tablou[a][b];
+    inf >> linie >> coloana;
 
-    for (int i = 0; i < a; i++)
+    int tablou[linie][coloana], steleStr = 0;
+
+     for (int i = 0; i < linie; i++)
     {
-        for (int j = 0; j < b; j++)
+        for (int j = 0; j < coloana; j++)
         {
             inf >> tablou[i][j];
         }
     }
-
-    for (int i = 0; i < a; i++)
+// rezolvarea a
+    for (int i = 0; i < linie; i++)
     {
-        for (int j = 0; j < b; j++)
+        for (int j = 0; j < coloana; j++)
         {
-            if(tablou[i][j] > tablou[i][j+1] && tablou[i][j] > tablou[i][j-1] &&
-            tablou[i][j] > tablou[i-1][j] && tablou[i][j] > tablou[i+1][j])
+            if(i == 0 && j == 0)
             {
-            cout << ": " << tablou[i][j] << endl;
+                if(tablou[i][j] > tablou[i+1][j] && tablou[i][j] > tablou[i][j+1] &&
+                   tablou[i][j] > tablou[i+1][j+1])
+                {
+                    cout << "Steaua: " << tablou[i][j] << endl;
+                    steleStr++;
+                    cout << "Pozitie i: " << i << " && " << "Pozitie j: " << j << endl;
+                }
+            }
+            else if(i == 0 && j == coloana-1)
+            {
+                if(tablou[i][j] > tablou[i-1][j] && tablou[i][j] > tablou[i][j-1] &&
+                   tablou[i][j] > tablou[i-1][j-1])
+                   {
+                       cout << "Steaua: " << tablou[i][j] << endl;
+                       steleStr++;
+                       cout << "Pozitie i: " << i << " && " << "Pozitie j: " << j << endl;
+                   }
+            }
+            else if(i == linie-1 && j == 0)
+            {
+                if(tablou[i][j] > tablou[i-1][j] && tablou[i][j] > tablou[i][j+1] &&
+                   tablou[i][j] > tablou[i-1][j+1])
+                   {
+                       cout << "Steaua: " << tablou[i][j] << endl;
+                       steleStr++;
+                       cout << "Pozitie i: " << i << " && " << "Pozitie j: " << j << endl;
+                   }
+            }
+            else if(i == linie-1 && j == coloana-1)
+            {
+                if(tablou[i][j] > tablou[i-1][j] && tablou[i][j] > tablou[i][j-1] &&
+                   tablou[i][j] > tablou[i-1][j-1])
+                   {
+                       cout << "Steaua: " << tablou[i][j] << endl;
+                       steleStr++;
+                       cout << "Pozitie i: " << i << " && " << "Pozitie j: " << j << endl;
+                   }
+            }
+            else
+            {
+                if(tablou[i][j] > tablou[i][j+1] && tablou[i][j] > tablou[i][j-1] &&
+                   tablou[i][j] > tablou[i-1][j] && tablou[i][j] > tablou[i+1][j] &&
+                   tablou[i][j] > tablou[i-1][j-1] && tablou[i][j] > tablou[i-1][j+1] &&
+                   tablou[i][j] > tablou[i+1][j+1] && tablou[i][j] > tablou[i+1][j-1])
+                {
+                    cout << "Steaua: " << tablou[i][j] << endl;
+                    steleStr++;
+                    cout << "Pozitie i: " << i << " && " << "Pozitie j: " << j << endl;
+
+                }
             }
         }
     }
+    cout << "Stele stralucitoare: " << steleStr;
 }
